aa656c23f23b27875755d65f5244624bc0599f30
Jeevanandham Ganesan <jeevanandham.ganesan@kanini.com>
2024-02-27 17:48:04 +0530

    code updated

--- celjangproj/settings.py
+++ celjangproj/settings.py
@@ -126,17 +126,13 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-
-
-
 # EMAIL SETTINGS
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jeeva13012002@gmail.com'
-EMAIL_HOST_PASSWORD ='sxydfthsikssznyd'
+EMAIL_HOST_PASSWORD ='mention your password'
 DEFAULT_FROM_EMAIL ='jeeva13012002@gmail.com'
 
 
